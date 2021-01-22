21adec08e4188dc87175e5a3194126debc0030d5
Callie Stump <callie@stu.mp>
2021-01-22 17:06:27 -0500

    adds Validations to Cats model

--- AnimalShelter/Models/Dog.cs
+++ AnimalShelter/Models/Dog.cs
@@ -8,10 +8,35 @@ namespace AnimalShelter.Models
   public class Dog
   {
     public int DogId { get; set; }
+
+
+    [Required]
+    [DisplayName("Dog Name")]
+    [StringLength(20, ErrorMessage = "15 Characters max")]
     public string dogName { get; set; }
+
+
+    [Required]
+    [DisplayName("Dog Gender")]
+    [StringLength(6, ErrorMessage = "6 Characters max")]
     public string dogGender { get; set; }
+
+
+    [Required]
+    [DisplayName("Dog Age")]
+    [StringLength(15, ErrorMessage = "15 Characters max")]
     public string dogAge { get; set; }
+
+
+    [Required]
+    [DisplayName("Dog Breed")]
+    [StringLength(15, ErrorMessage = "20 Characters max")]
     public string dogBreed { get; set; }
+
+
+    [Required]
+    [DisplayName("Dog Description")]
+    [StringLength(500, ErrorMessage = "500 Characters max")]
     public string dogDescription { get; set; }
   }
 }
